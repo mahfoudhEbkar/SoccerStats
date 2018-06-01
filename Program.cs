f29dfb506b7a52356caf50344c29c5bd266259a0
mahfoudhebkar <dah.ebkre@gmail.com>
2018-06-01 18:42:51 -0400

    Reading line by line

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 
@@ -9,44 +10,55 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
-             
             string currentDirectory = Directory.GetCurrentDirectory();
             //DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(currentDirectory, "SoccerGameResults.csv");
-            var file  = new FileInfo(fileName);
+            var fileContents = ReadResults(fileName);
 
-            if (file.Exists)
-            {
-                //using (var reader = new StreamReader(file.FullName))
-                //{
-                //  Console.SetIn(reader);
-                // Console.WriteLine(Console.ReadLine());
+            //var file  = new FileInfo(fileName);
 
-                //reader.Close();
-                //}
-                string[] result = ReadFile(file.FullName).Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var line in result)
-                {
-                    Console.WriteLine(line);
-                    //Console.WriteLine("Spaaaaaaaaaaaaace");
+            //if (file.Exists)
+            //{
+            //using (var reader = new StreamReader(file.FullName))
+            //{
+            //  Console.SetIn(reader);
+            // Console.WriteLine(Console.ReadLine());
 
-                }//Console.ReadLine();
-            }
+            //reader.Close();
+            //}
+            //string[] result = ReadFile(file.FullName).Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var line in result)
+            //{
+            //    Console.WriteLine(line);
 
-            
-        
+            //}
+            //string[] result
+            //}
         }
 
         public static string ReadFile(string fileName)
-        {
-             
+        {  
             {
+                //Classes such as Stream, StreamReader, StreamWriter etc implements IDisposable
                 using (var reader = new StreamReader(fileName))
                 {
                     return reader.ReadToEnd();
                 }  
+            }    
+        }
+
+        public static List<string[]> ReadResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                //while((reader.ReadLine()) != null )
+                while (reader.Peek() > -1)
+                {
+                    soccerResults.Add(reader.ReadLine().Split(','));
+                }
             }
-             
+            return soccerResults;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SoccerStats
+
+<b>Streams and Data Processing</b>
+
+ reading, writing  and processing  data using streams in C#, 
+ doing file system operations.
+also doing serialization 
+ with JSON using Json.NET, and how to send web requests from our application.
